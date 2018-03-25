68c56f364ca96dfa1c1eae6d91aa865a492fdc18
Sam <sam@sasbartlett.com>
2018-03-25 15:44:42 -0500

    handle annoying kitsu logic

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ const PUBG_API_URL = "https://api.playbattlegrounds.com";
 
 export class pubgApiWrapper {
     constructor(token) {
+        this.token = token;
+        this.updateBaseURL(PUBG_API_URL)
+    }
+
+    updateBaseURL(url) { 
         this.api = new Kitsu({
-            baseURL: PUBG_API_URL,
+            baseURL: url,
             headers: {
-                Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${this.token}`,
                 Accept: 'application/vnd.api+json'
             },
             pluralize: false
@@ -16,22 +21,25 @@ export class pubgApiWrapper {
     }
 
     getStatus() {
+        this.updateBaseURL(PUBG_API_URL);
         return this.api.get('status').then(({data}) => data, (err) => {
             throw err;
         })
     }
 
     getMatches(shard, filters = {}, offset=0, limit=5, sort="createdAt") {
+        this.updateBaseURL(`${PUBG_API_URL}/shards/${shard}`);
         let params = { filter: filters, sort: sort };
         if(offset != 0 && limit != 0) params.page = {limit: limit, offset: offset};
 
-        return this.api.get(`shards/${shard}/matches`, params).then(({data}) => data, err => {
+        return this.api.get(`matches`, params).then(({data}) => data, err => {
             throw err;
         })
     }
 
     getMatch(shard, id) {
-        return this.api.get(`shards/${shard}/matches/${id}`).then(({data}) => data, err => {
+        this.updateBaseURL(`${PUBG_API_URL}/shards/${shard}`);
+        return this.api.get(`matches/${id}`).then(({data}) => data, err => {
             throw err;
         })
     }
